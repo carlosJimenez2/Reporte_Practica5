40eaf8442ba7c0e6c6b430e227ef64d608550b90
carlosJimenez2 <42255224+carlosJimenez2@users.noreply.github.com>
2018-09-05 17:10:14 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace excepciones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int denominador = 0;
+            int numerador = 0;
+            int resultado = 0;
+
+            try
+            {
+                Console.WriteLine("Ingrese un valor");
+                numerador = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Ingrese un segundo valor");
+                denominador = Convert.ToInt32(Console.ReadLine());
+                resultado = numerador / denominador;
+                Console.WriteLine("El resultado es = " + resultado);
+                Console.ReadKey();
+               
+            }
+            catch (DivideByZeroException error)
+            {
+                Console.WriteLine(error.Message);
+                Console.WriteLine("Intente introducir nuevos valores diferentes de cero");
+            }
+            catch(FormatException error)
+            {
+                Console.WriteLine(error.Message);
+                Console.WriteLine("Intente introducir valores que se solicitan");
+
+            }
+            catch(OverflowException error)
+            {
+                Console.WriteLine(error.Message);
+                Console.WriteLine("Intente introducir valores dentro del rango");
+            }
+            Console.ReadKey();
+        }
+    }
+}
